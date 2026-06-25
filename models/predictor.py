6a05ec5c9588cd19d35e1497fb680f897eefe941
Ben <benyebai@gmail.com>
2026-06-24 23:55:00 -0700

    fixed postiional embedding implementation

--- models/predictor.py
+++ models/predictor.py
@@ -3,13 +3,17 @@ import torch.nn as nn
 
 
 # our embedding from our video encoder is [128, 4, 192]
-class Predictor(nn.Module):
-    def __init__(self, T=4, num_frame=4, input_dim=192):
+class ARPredictor(nn.Module):  # autoregressive predictor
+    def __init__(self, num_frame=4, input_dim=192):
         super().__init__()
 
         # this is for adding our position embedding, so each 128 batches has a group of 4
         # we want these 4 to learn the ordering, so we will create a 4 different 192 vectors
-        self.positional_emb = nn.Parameter(torch.rand(1, num_frame, input_dim))
+        self.positional_emb = nn.Parameter(torch.randn(1, num_frame, input_dim))
 
     def forward(self, x):
-        x = x + self.positional_emb[:, : self.T]
+        T = x.size(1)  # get the T from the input
+        # just means get everything but for the middle dim get the first T
+        x = x + self.positional_emb[:, :T]
+
+        return x
