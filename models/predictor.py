2dde2fe92491f59cc33292a00ea687b112e97149
Ben <benyebai@gmail.com>
2026-06-25 00:37:43 -0700

    update some more comments

--- models/predictor.py
+++ models/predictor.py
@@ -9,6 +9,7 @@ class ARPredictor(nn.Module):  # autoregressive predictor
 
         # this is for adding our position embedding, so each 128 batches has a group of 4
         # we want these 4 to learn the ordering, so we will create a 4 different 192 vectors
+        # and then use it for all batches
         self.positional_emb = nn.Parameter(torch.randn(1, num_frame, input_dim))
         self.dropout = nn.Dropout(p=0.1)
 
@@ -16,5 +17,8 @@ class ARPredictor(nn.Module):  # autoregressive predictor
         T = x.size(1)  # get the T from the input
         # just means get everything but for the middle dim get the first T
         x = x + self.positional_emb[:, :T]
+        # this is the dropout, we just zero out some of our embedding tensors
+        # it might learn based on just our data, we need it to learn deeply
+        # get to an answer from multiple routes
         x = self.dropout(x)
         return x
