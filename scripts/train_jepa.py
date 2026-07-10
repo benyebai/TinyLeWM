349cf012845b0bab6c54a8c92d83926544d5f186
Ben <benyebai@gmail.com>
2026-07-10 02:33:44 -0700

    fix

--- scripts/train_jepa.py
+++ scripts/train_jepa.py
@@ -1,4 +1,6 @@
 # will use all the stuff we have in models and our dataset to run the training loop
+import torch
+import torch.nn.functional as F
 
 from models.jepa import Jepa
 from models.sigreg import SigReg
@@ -14,9 +16,9 @@ def train_step(jepa: Jepa, sigreg: SigReg, pixels, actions, lmbda=0.1):
 
     # Pred loss -- MSE, matching time time steps
     # t0 |t1 t2 t3| vs |t1 t2 t3| t4
-    pred_loss = (next_emb[:, 1:] - emb[:, :-1]).squared()
+    pred_loss = F.mse_loss(next_emb[:, :-1], emb[:, 1:])
     # needs to be in [T, B, D]
-    sigreg_loss = sigreg.forward(emb.transpose(0, 1))
+    sigreg_loss = sigreg(emb.transpose(0, 1))
     total_loss = pred_loss + lmbda * sigreg_loss
 
     return total_loss, pred_loss, sigreg_loss
