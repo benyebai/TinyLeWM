2b91fa12ff78d528c99e57ab60b09d41df7c4d61
Ben <benyebai@gmail.com>
2026-07-09 21:40:49 -0700

    sigreg values

--- models/sigreg.py
+++ models/sigreg.py
@@ -1,3 +1,5 @@
+from numpy import exp
+import torch
 import torch.nn as nn
 
 # Every world model loss function is trying to avoid representation collapse and lop sidedness
@@ -5,7 +7,7 @@ import torch.nn as nn
 # SIGReg now what is it? In my understanding lets say u have an array of numbers, to figure out if its gaussian
 # if u only calculate the mean and the sd, and look for if its close to 0 or 1, u can get tricked [-1, -1, 1, 1] (not gaussian but satisfies the requirements)
 # so what we do instead is fit it using the functions cos(t * x) / sin(t * x), then the average is exp(-t**2 / 2) if its gaussian
-# and u want to do that accross all dimensions
+# and u want to do that accross all directions
 #
 # For terminology: characteristic function - thats the cos average or the sin average
 #                  epps-pulley test - comparing it to exp(-t**2 / 2)
@@ -19,6 +21,14 @@ class SigReg(nn.Module):
     # t: the actual values of the t's
     def __init__(self, knots=17, num_arrows=1024):
         super().__init__()
+        self.t = torch.linspace(0, 3, knots)
+        self.dt = 3 / (knots - 1)
+        self.window = exp(-self.t**2 / 2) # the phi
+        self.weights = [2*self.dt]*knots
+        self.weights[0], self.weights[1] = self.dt, self.dt
 
 
     def forward(self, emb):
+        # for each arrow through the cloud of embeddings
+        #   do the dot product for each embedding
+        #       sum [B, T, 192], for each of the embeddings cos(t * x) / BxT for the average
